fbf0d82f1fd615e01e58d67efc3d1e4ac6500043
suea724 <suea724@gmail.com>
2022-03-28 20:47:43 +0900

    클래스 생성자 코드 리뷰 반영

--- java/assignment/13_class/03/Student.java
+++ java/assignment/13_class/03/Student.java
@@ -1,6 +1,30 @@
 package com.test.java.obj.question.q3;
 
 class Student {
+	
+	/**
+	 * 생성자 오버로딩을 구현하시오.
+		- Student
+			- 학생 클래스
+			- 상태
+				- name: 이름
+				- age: 나이
+				- grade: 학년
+				- classNumber: 반
+				- number: 번호
+			- 행동
+				- public Student()
+					- 기본 생성자
+					- 이름("미정"), 나이(0), 학년(0), 반(0), 번호(0)
+				- public Student(String name, int age, int grade, int classNumber, int number)
+					- 생성자 오버로딩
+				- public Student(String name, int age)
+					- 생성자 오버로딩
+				- public Student(int grade, int classNumber, int number)
+					- 생성자 오버로딩
+				- String info()
+					- 이름(나이 : 0세, 학년 : 0, 반 : 0, 번호 : 0)
+	 */
 
 	private String name;
 	private int age;
@@ -28,6 +52,8 @@ class Student {
 		this.classNumber = classNumber;
 		this.number = number;
 	}
+	
+	// 형식문자의 형식에 맞게 자동으로 형변환이 이루어진다! => toString()을 따로 사용해주지 않아도 자동으로 실행됨
 	
 	public String info() {
 		return String.format("%s(나이: %s, 학년: %s, 반: %s, 번호: %s)"
--- java/assignment/13_class/03/Time.java
+++ java/assignment/13_class/03/Time.java
@@ -2,6 +2,35 @@ package com.test.java.obj.question.q3;
 
 class Time {
 	
+	/**
+	 * 생성자 오버로딩을 구현하시오.
+		- 2자리 출력
+		- Time
+			- 시간 클래스
+			- 상태
+				- hour: 시(0 이상 양의 정수)
+				- minute: 분(0 이상 양의 정수)
+				- second: 초(0 이상 양의 정수)
+			- 행동
+				- public Time()
+					- 기본 생성자
+					- 시(0), 분(0), 초(0)
+				- public Time(int hour, int minute, int second)
+					- 생성자 오버로딩
+					- hour : 시
+					- minute : 분
+					- second : 초
+				- public Time(int minute, int second)
+					- 생성자 오버로딩
+					- minute : 분
+					- second : 초
+				- public Time(int second)
+					- 생성자 오버로딩
+					- second : 초
+				- String info()
+					- 02:30:45
+	 */
+	
 	// 두자리씩 출력
 	// 양의 정수 -> 생성자에서 유효성 검사
 	// 시간 올림
@@ -23,10 +52,25 @@ class Time {
 	
 	public Time (int hour, int minute, int second) {
 		
-		this.second = second % 60; // 초를 60으로 나눈 나머지
-		this.minute = (minute + second / 60) % 60; // (초를 60으로 나눈 몫 + minute)의 나머지
-		this.hour = hour + (minute + second / 60) / 60; // (초를 60으로 나눈 몫 + minute)의 몫 + hour
+		if(validTime(hour, minute, second)) {
+			this.second = second % 60; // 초를 60으로 나눈 나머지
+			this.minute = (minute + second / 60) % 60; // (초를 60으로 나눈 몫 + minute)의 나머지
+			this.hour = hour + (minute + second / 60) / 60; // (초를 60으로 나눈 몫 + minute)의 몫 + hour
+		} else {
+			System.out.println("시간은 양수로 입력해주세요.");
 		}
+		
+	}
+	
+	// 유효성 검사 부분 추가
+	private boolean validTime(int hour, int minute, int second) {
+		
+		if (hour < 0 || minute < 0 || second < 0) {
+			return false;
+		}
+		
+		return true;
+	}
 	
 	public String info() {
 		return String.format("%02d:%02d:%02d", hour, minute, second);
